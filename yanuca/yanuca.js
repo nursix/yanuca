a72c2bf8dbeb5250eb9adcfd82228278ca5db21f
Dominic König <dominic@nursix.org>
2013-01-02 20:38:55 +0100

    Version 1.3.0 (new license)

--- yanuca/yanuca.js
+++ yanuca/yanuca.js
@@ -1,29 +1,26 @@
-// ****************************************************************************
-// * YaNuCa Yet Another Nutrition Calculator                                  *
-// * (c) 2003-2009 by Dominic König (nursix.org)                              *
-// ****************************************************************************
-// * yanuca.js : Core Elements of YaNuCa                                      *
-// ****************************************************************************
-// * This program is free software: you can redistribute it and/or modify     *
-// * it under the terms of the GNU Affero General Public License as published *
-// * by the Free Software Foundation, either version 3 of the License, or     *
-// * (at your option) any later version.                                      *
-// *                                                                          *
-// * This program is distributed in the hope that it will be useful,          *
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
-// * GNU Affero General Public License for more details.                      *
-// *                                                                          *
-// * You should have received a copy of the GNU Affero General Public License *
-// * along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
-// *                                                                          *
-// * For more information about nursix.org, please visit us at                *
-// * my website at http://www.nursix.org                                      *
-// ****************************************************************************
+/**
+   YaNuCa (Yet Another NUtrition CAlculator) Version 1.3
 
-// ****************************************************************************
-// Global settings
-//
+   (c) 2003-2013 Dominic König (nursix.org)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+   of the Software, and to permit persons to whom the Software is furnished to do
+   so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
 
 // Illness factor (must be in range 0.5...1.4)
 var YA_ILLNESS_FACTOR = {
